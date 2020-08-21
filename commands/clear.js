1b54477f61e92df8863052707f82fba1825b8b1d
Chad Altemose <chad@chadzilla.com>
2020-08-21 08:25:20 -0400

    Added clear command to clear channel messages

---
+++ commands/clear.js
@@ -0,0 +1,24 @@
+const ADMINISTRATOR = 'ADMINISTRATOR';
+
+module.exports = {
+	name: 'clear',
+	description: 'Clear the current channel.',
+	cooldown: 5,
+	execute: async (message, args, keyv, prefix, guildId) => {
+		if (!message.member.hasPermission(ADMINISTRATOR)) {
+			return message.channel.send('You do not have permissions to use the `clear` command.');
+		}
+
+		const deleteCount = parseInt(args[0], 10);
+
+		if(!deleteCount || deleteCount < 2 || deleteCount > 100) {
+			return message.channel.send("Please provide a number between 2 and 100 for the number of messages to delete");
+		}
+
+		const fetched = await message.channel.messages.fetch({limit: deleteCount});
+		message.channel
+			.bulkDelete(fetched)
+			.catch(error => message.reply(`Couldn't delete messages: ${error}`));
+		
+	},
+};
