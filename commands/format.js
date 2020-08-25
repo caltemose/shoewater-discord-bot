c0f7712479515871d536c4ce8dba5accd2eae270
Chad Altemose <chad@chadzilla.com>
2020-08-25 10:31:43 -0400

    added format command for testing issues with message rendering and discord "markup"

---
+++ commands/format.js
@@ -0,0 +1,37 @@
+const { ADMINISTRATOR } = require('../helpers/constants');
+
+module.exports = {
+	name: 'format',
+	description: 'message format testing',
+	cooldown: 1,
+	execute: async (message, args, keyv, prefix, guildId) => {
+		if (!message.member.hasPermission(ADMINISTRATOR)) {
+			return message.channel.send('You do not have permissions to use the `format` command.');
+		}
+
+		let msg;
+		const subcommand = args[0];
+		if (!subcommand) {
+			msg = 'a_user\nanother_user\na_third__user\nand-a__fourth';
+			return message.channel.send(msg);
+		}
+		else {
+			switch (subcommand) {
+				case 'esc':
+					msg = 'a\_user\nanother\_user\na\_third\_\_user\nand-a\_\_fourth';
+					break;
+				case 'code':
+					msg = '```\na_user\nanother_user\na_third__user\nand-a__fourth\n```';
+					break;
+				case 'single':
+					msg = 'a_single_underscore\n_single_';
+					break;
+				default:
+					msg = 'unrecognized subcommand';
+					break;
+			}
+			console.log(msg);
+			return message.channel.send(msg);
+		}
+	},
+};
