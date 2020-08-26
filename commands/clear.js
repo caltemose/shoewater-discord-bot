e77e0b1d9e07f4943cb4c9f518ae33b7d662afe6
Chad Altemose <chad@chadzilla.com>
2020-08-26 08:59:58 -0400

    Added utils with logging function; added logging to clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,5 @@
 const { ADMINISTRATOR } = require('../helpers/constants');
+const { logError, getISOStamp, getNameFromMessage } = require('../helpers/utils');
 
 module.exports = {
 	name: 'clear',
@@ -6,19 +7,23 @@ module.exports = {
 	cooldown: 5,
 	execute: async (message, args, keyv, prefix, guildId) => {
 		if (!message.member.hasPermission(ADMINISTRATOR)) {
+			logError(`'${getNameFromMessage(message)}' tried to access the 'clear' command`, getISOStamp());
 			return message.channel.send('You do not have permissions to use the `clear` command.');
 		}
 
 		const deleteCount = parseInt(args[0], 10);
 
 		if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
+			logError(`'${getNameFromMessage(message)}' used 'clear' command with deleteCount of ${deleteCount}`, getISOStamp());
 			return message.channel.send("Please provide a number between 2 and 100 for the number of messages to delete");
 		}
 
 		const fetched = await message.channel.messages.fetch({limit: deleteCount});
 		message.channel
 			.bulkDelete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages: ${error}`));
-		
+			.catch(error => {
+				logError(`bulkDelete error at ${getISOStamp()}`, error);
+				message.reply(`Couldn't delete messages: ${error}`);
+			});
 	},
 };
---
+++ helpers/utils.js
@@ -0,0 +1,20 @@
+const getDivider = size => {
+	let div = '';
+	for(let i = 0; i<size; i++) {
+		div += '-';
+	}
+	return `\n${div}\n`;
+}
+
+module.exports = {
+	logError: function () {
+		let msg = getDivider(80) + Array.from(arguments).join('\n') + getDivider(70);
+		console.error(msg);
+	},
+
+	getISOStamp: (dateArg) => {
+		return dateArg ? dateArg.toISOString() : new Date().toISOString();
+	},
+
+	getNameFromMessage: message => message.member.nickname || message.author.username
+};
