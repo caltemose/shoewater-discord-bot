c3f8c386434b5c5194c91e42cd4b85032e152adb
Chad Altemose <chad@chadzilla.com>
2020-11-18 16:46:55 -0500

    logging added to dayone command

--- commands/dayone.js
+++ commands/dayone.js
@@ -6,14 +6,15 @@ module.exports = {
 	description: 'Day One Raid commands',
 	cooldown: 1,
 	execute: async (message, args, keyv, prefix, guildId) => {
-		console.log('member.nickname', message.member.nickname, 'author.username', message.author.username);
-		const myDiscordId = message.member.nickname || message.author.username;
+		// console.log('member.nickname', message.member.nickname, 'author.username', message.author.username);
 		var raidDayOne = await keyv.get('raidDayOne');
+		const myDiscordId = message.member.nickname || message.author.username;
 		if (!raidDayOne) {
 			raidDayOne = {};
 		}
 		const subcommand = args.shift();
 		if (subcommand === 'join') {
+			logger.log(`$dayone join from ${myDiscordId}`);
 			raidDayOne.push(myDiscordId);
 			await keyv.set('raidDayOne', raidDayOne);
 			return message.channel.send(myDiscordId + ' wants to join the raid.');
@@ -21,16 +22,19 @@ module.exports = {
 		else if (subcommand === 'remove') {
 			const found = raidDayOne.indexOf(myDiscordId);
 			if (found > -1) {
+				logger.log(`$dayone remove from ${myDiscordId}`);
 				raidDayOne.splice(found, 1);
 				await keyv.set('raidDayOne', raidDayOne);
 				return message.channel.send(myDiscordId + ' wants out of the raid squad.');
 			}
 			else {
+				logger.log(`$dayone illegal remove from ${myDiscordId}`);
 				return message.channel.send('You were not in the raid so no need to remove yourself.');
 			}
 		}
 		else if (subcommand === 'clear') {
 			if (message.member.hasPermission(ADMINISTRATOR)) {
+				logger.log(`$dayone clear from ${myDiscordId}`);
 				await keyv.set('raidDayOne', []);
 				return message.channel.send('cleared the raid list');
 			}
@@ -40,16 +44,19 @@ module.exports = {
 			msg += '`$dayone` -> this will show the list of day one raiders\n';
 			msg += '`$dayone join` -> this will add you to the list of day one raiders\n';
 			msg += '`$dayone remove` -> this will remove you from the list of day one raiders\n';
+			logger.log(`$dayone help from ${myDiscordId}`);
 			return message.channel.send(msg);
 		}
 		else if (subcommand === 'message') {
 			if (message.member.hasPermission(ADMINISTRATOR)) {
+				logger.log(`$dayone message from ${myDiscordId}`);
 				let msg = '';
 				raidDayOne.forEach(member => msg += `@${member} `);
 				return message.channel.send(msg);
 			}
 		}
 		else {
+			logger.log(`$dayone (list) from ${myDiscordId}`);
 			let msg = '**Members in day one Raid:**\n';
 			raidDayOne.forEach(member => msg += ` > ${member}\n`);
 			return message.channel.send(msg);
